7b69f71453b3815da25b32583a92908f08e60040
Lukáš Trkan <lukas@trkan.cz>
2024-10-10 23:36:39 +0200

    fix js

--- js/src/admin/index.js
+++ js/src/admin/index.js
@@ -1,6 +1,6 @@
 import app from 'flarum/app';
 import MybbToFlarumPage from './components/MybbToFlarumPage';
 
-app.initializers.add('michaelbelgium-mybb-to-flarum', () => {
-    app.extensionData.for('michaelbelgium-mybb-to-flarum').registerPage(MybbToFlarumPage);
+app.initializers.add('lukastrkan-mybb-to-flarum', () => {
+    app.extensionData.for('lukastrkan-mybb-to-flarum').registerPage(MybbToFlarumPage);
 });
